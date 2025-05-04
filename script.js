bd4d5561bbd6173f5a6e96666515949c99244e0c
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-04 23:02:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let codes=document.querySelectorAll(".code");
 codes[0].focus();
 codes.forEach((code,idx)=>{
 code.addEventListener("keydown",(e)=>{
-	if(e.key>=0 && e.key<=9){
+	if(e.key>=0 && e.key<=9 && idx<5 ){
 		setTimeout(()=>codes[idx+1].focus(),10);
 	}else if(e.key==="Backspace"){
 		if(idx>0)
