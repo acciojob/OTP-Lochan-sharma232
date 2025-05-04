075b38a90ff042b38bee9a49b99a99249df9cbfe
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-04 15:49:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-let codes=document.querySelectorAll(".code");
-codes[0].focus();
-cod
+// let codes=document.querySelectorAll(".code");
+// codes[0].focus();
+// cod
